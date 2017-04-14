5217274f96804cbe36a090ee1844c0fac63450e8
Alayna Buysse <alaynagrace@comcast.net>
2017-04-14 15:53:05 -0500

    finished pro_practice

--- pro_practice/script.js
+++ pro_practice/script.js
@@ -10,13 +10,27 @@
 */
 $(document).ready(function(){
   var counter = $('.count');
+  var imageArray = ["https://cdn.meme.am/instances/55130291.jpg","https://cdn.meme.am/cache/instances/folder514/65272514.jpg",
+"http://i3.kym-cdn.com/photos/images/facebook/000/638/746/c3d.png","http://i2.kym-cdn.com/photos/images/facebook/000/330/923/d5e.jpg",
+"http://i1.kym-cdn.com/photos/images/original/000/402/782/1d4.gif"];
   setInterval(function(){
     if(parseInt(counter.text()) !== 0){
       counter.text(parseInt(counter.text())-1);
     }
     else{
       counter.text(0);
+    var rand = Math.floor(Math.random() * imageArray.length);
+    $('.imgDiv').empty();
+    $('.imgDiv').append("<img src='"+imageArray[rand]+"'>");
     }
   },1000);
-  
+
+  $('.reset').on('click',function(){
+    counter.text(10);
+  });
+
+  if(parseInt(counter.text()) === 0){
+    var rand = Math.floor(Math.random() * imageArray.length);
+    $('.imgDiv').append("<img src='"+imageArray[rand]+"'>");
+  }
 });
